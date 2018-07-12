6b286a665f0c1448b61cd918ee6325d7b25833f8
Curtis Cawley <curtis.cawley@icloud.com>
2018-07-12 16:49:33 -0700

    changed board game from hard coded to dynamically generated

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,19 @@
-const breakLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
-console.log('This is what an empty board would look like:');
-console.log(breakLine);
-console.log(breakLine);
-console.log(breakLine);
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(breakLine);
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
